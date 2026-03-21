ab6d28213eb8eeba9415cc9345c15db425593f86
Matt Pilon <pilon.matthieu@gmail.com>
2026-03-21 14:51:15 -0400

    [BUG] - Fix targeting after Ability selected

---
+++ Source/MannequinnFantasy/MannequinnFantasy.Build.cs
@@ -0,0 +1,51 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MannequinnFantasy : ModuleRules
+{
+	public MannequinnFantasy(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"MannequinnFantasy",
+			"MannequinnFantasy/Variant_Platforming",
+			"MannequinnFantasy/Variant_Platforming/Animation",
+			"MannequinnFantasy/Variant_Combat",
+			"MannequinnFantasy/Variant_Combat/AI",
+			"MannequinnFantasy/Variant_Combat/Animation",
+			"MannequinnFantasy/Variant_Combat/Gameplay",
+			"MannequinnFantasy/Variant_Combat/Interfaces",
+			"MannequinnFantasy/Variant_Combat/UI",
+			"MannequinnFantasy/Variant_SideScrolling",
+			"MannequinnFantasy/Variant_SideScrolling/AI",
+			"MannequinnFantasy/Variant_SideScrolling/Gameplay",
+			"MannequinnFantasy/Variant_SideScrolling/Interfaces",
+			"MannequinnFantasy/Variant_SideScrolling/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/MannequinnFantasy/MannequinnFantasy.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MannequinnFantasy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MannequinnFantasy, "MannequinnFantasy" );
+
+DEFINE_LOG_CATEGORY(LogMannequinnFantasy)
